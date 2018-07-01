5529ab770d20d6b29f57ec708b04035885dec9a8
marco <marco.minarelli#stud.unifi.it>
2018-07-01 17:39:01 +0200

    Corrected CMakeLists and AcitivityLists tests

--- test/ActivityListFixture.cpp
+++ test/ActivityListFixture.cpp
@@ -1,5 +1,7 @@
-#include "gtest/gtest.h"
+#include <string>
+#include <memory>
 
+#include "gtest/gtest.h"
 
 #include "../src/ActivityList.h"
 #include "../src/Commitment.h"
@@ -9,33 +11,46 @@
 class ActivityListSuite : public ::testing::Test {
 
 protected:
+
+    ActivityList* al;
+    ActivityList* anotherAl;
+    
     virtual void SetUp() {
-        Commitment c(Date(12, 01, 18), Date(14, 01, 18), Time(10, 00), Time(12,00), false, "This is a note", "This is a description", "www.loremipsum.com");
-        al.addCommitment(c);
+    	al = new ActivityList("Test", "ActivityTest");
+    	anotherAl = new ActivityList("Al", "Another al");
+    	
+    	
+        Commitment c(Date(12, 01, 18), Date(14, 01, 18), Time(static_cast<unsigned>(10), static_cast<unsigned>(00), static_cast<unsigned>(00)), Time(static_cast<unsigned>(12),static_cast<unsigned>(00), static_cast<unsigned>(00)), false, std::string("This is a note"), 
+       std::string("www.loremipsum.com"));
+        al->addCommitment(c);
+    }
+    
+    virtual void TearDown(){
+    	delete al;
+    	delete anotherAl;
     }
 
-    ActivityList al("Test", "ActivityTest");
-    ActivityList anotherAl("Al", "Another al");
+    
 };
 
 
-TEST_F(ActivityList, Constructor) {
-    ASSERT_STREQ("Test", al.getName());
-    ASSERT_STREQ("ActivityTest", al.getName());
+TEST_F(ActivityListSuite, Constructor) {
+    ASSERT_STREQ("Test", al->getName().c_str());
+    ASSERT_STREQ("ActivityTest", al->getDescription().c_str());
 }
 
-TEST_F(ActivityList, GetCommitments) {
-    ASSERT_EQ(1, al.getCommitments().size());
+TEST_F(ActivityListSuite, GetCommitments) {
+    ASSERT_EQ(1, al->getCommitments().size());
     
-    Commitment c(Date(14, 10, 18), Date(14, 10, 18), Time(14, 00), Time(16,00), false, "This is a note, again", "This is another description", "www.boh.com");
-        al.addCommitment(c);
+    Commitment c(Date(14, 10, 18), Date(14, 10, 18), Time(static_cast<unsigned>(14), static_cast<unsigned>(00), static_cast<unsigned>(00)), Time(static_cast<unsigned>(60),static_cast<unsigned>(00), static_cast<unsigned>(00)), false, std::string("This is a note, again"), std::string("www.boh.com"));
+        al->addCommitment(c);
     
-    ASSERT_EQ(2, al.getCommitments().size());
+    ASSERT_EQ(2, al->getCommitments().size());
 }
 
-TEST_F(ActivityList, GetCommitments) {
-    ASSERT_TRUE(al == al);
+TEST_F(ActivityListSuite, EqualityOperator) {
+    ASSERT_TRUE(*al == *al);
     
-    ASSERT_FALSE(al == anotherAl);
+    ASSERT_FALSE(*al == *anotherAl);
 }
 
